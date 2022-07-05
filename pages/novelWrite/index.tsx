4092e1eaa8299cabd8fae0fbe03fec6ca500f667
songinho96 <inhosong96@gmail.com>
2022-07-05 20:26:57 +0900

    novelWritePage

---
+++ pages/novelWrite/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import NovelWriteContainer from "../../src/components/units/novelWrite/NovelWrite.container";
+
+export default function NovelWritePage() {
+  return <NovelWriteContainer />;
+}
---
+++ src/components/units/novelWrite/NovelWrite.styles.ts
@@ -0,0 +1,147 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 1200px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MainTitle = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+  color: #747474;
+  border-bottom: 1px solid black;
+  padding-bottom: 30px;
+  padding-top: 25px;
+`;
+
+export const WrapperBody = styled.div`
+  padding: 0 50px;
+`;
+
+export const WrapperLavel = styled.div`
+  padding: 40px 0;
+  border-bottom: 1px solid #b1b1b1;
+`;
+
+export const WarningText = styled.div`
+  font-size: 16px;
+  color: #b1b1b1;
+`;
+
+export const WrapTitle = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const TitleLabel = styled.input`
+  ::placeholder {
+    color: black;
+  }
+  width: 100%;
+  font-size: 24px;
+  font-weight: bold;
+  border: none;
+`;
+
+export const Label = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const InputTitle = styled.input`
+  width: 800px;
+  height: 60px;
+  margin-left: 30px;
+`;
+
+export const WrapGenreCycle = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const WrapGenre = styled.div`
+  display: flex;
+`;
+
+export const WrapSelect = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-left: 60px;
+  border: 1px solid #b1b1b1;
+  width: 230px;
+  height: 40px;
+`;
+
+export const GenreLabel = styled.div`
+  font-size: 18px;
+`;
+
+export const SelectButton = styled.img`
+  width: 24px;
+  height: 24px;
+`;
+
+export const WrapCycle = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const CheckButton = styled.img`
+  margin: 0 10px;
+`;
+
+export const CycleLabel = styled.div`
+  font-size: 15px;
+`;
+
+export const WrapCycleButton = styled.div`
+  margin-left: 15px;
+  display: flex;
+`;
+
+export const CycleButton = styled.div`
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  font-size: 15px;
+  border: 1px solid #b1b1b1;
+`;
+
+export const WrapCoverImage = styled.div`
+  display: flex;
+`;
+
+export const CoverImage = styled.img`
+  margin-left: 100px;
+  width: 150px;
+  height: 210px;
+`;
+
+export const WrapIntroduce = styled.div`
+  padding: 40px 0;
+`;
+
+export const IntroduceInput = styled.input`
+  margin-top: 40px;
+  width: 100%;
+  height: 440px;
+`;
+
+export const WrapSubmitButton = styled.div`
+  padding: 70px 0;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ClickButton = styled.button`
+  margin: 0 50px;
+  background-color: #2277d8;
+  color: white;
+  text-align: center;
+  width: 160px;
+  height: 60px;
+  border: none;
+  font-size: 20px;
+`;
