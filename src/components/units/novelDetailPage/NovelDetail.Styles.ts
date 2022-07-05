0c2f74681f0dc5a4b178563f09592ca6686e1fe2
hskwon5170 <hskwon517@gmail.com>
2022-07-05 20:38:35 +0900

    :등록:novelDetail

---
+++ src/components/units/novelDetailPage/NovelDetail.Styles.ts
@@ -0,0 +1,286 @@
+import styled from "@emotion/styled";
+
+export const Genre = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const SelectAll = styled.div`
+  font-size: 12px;
+  margin-top: 9px;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+export const DIV = styled.div`
+  display: flex;
+`;
+
+export const TopFont = styled.div`
+  font-size: 15px;
+`;
+
+export const NovelTitle = styled.div`
+  font-size: 22px;
+  font-weight: 600;
+  margin: 30px 0px 10px 0px;
+`;
+
+export const StarImg = styled.img`
+  width: 13px;
+  height: 13px;
+`;
+
+export const Score = styled.div`
+  font-size: 15px;
+  margin-left: 10px;
+`;
+
+export const StarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0px 0px 10px 0px;
+`;
+
+export const WriterImg = styled.img`
+  width: 33px;
+  height: 33px;
+`;
+
+export const NovelRemarks = styled.div`
+  font-size: 15px;
+  color: #757575;
+  margin-top: 10px;
+`;
+
+export const WriterFont = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  margin: 0px 10px 0px 10px;
+`;
+
+export const NovelImage = styled.img`
+  width: 275px;
+  height: 292px;
+`;
+
+export const SmallArrow = styled.img`
+  width: 15px;
+  height: 14px;
+  margin: 0px 10px 0px 10px;
+`;
+
+export const TopWrapper = styled.div`
+  /* display: flex; */
+`;
+
+export const ColumnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+`;
+
+export const FlexWrapper = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
+
+export const Divs = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 38px;
+  height: 20px;
+  background-color: #d9d9d9;
+  font-size: 15px;
+  font-weight: 600;
+`;
+
+export const HeartImg = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+
+export const HeartCount = styled.div`
+  font-size: 22px;
+  margin-left: 10px;
+`;
+
+export const ProfileWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Line = styled.div`
+  border-bottom: 1px solid black;
+  /* width: 826px; */
+`;
+
+export const ArrowImg = styled.img`
+  height: 11px;
+`;
+
+export const New = styled.div`
+  font-size: 12px;
+`;
+
+export const All = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+export const Sum = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  color: #00a0e9;
+  margin-left: 10px;
+`;
+
+export const MarginWrapper = styled.div`
+  margin-left: 700px;
+  display: flex;
+  align-items: center;
+`;
+
+export const VerticalLine = styled.div`
+  border-right: 1px solid black;
+  height: 12.5px;
+  margin-top: 22px;
+  margin-left: 10px;
+`;
+
+export const Title = styled.div`
+  font-size: 15px;
+`;
+
+export const Look = styled.div`
+  width: 65px;
+  height: 30px;
+  background-color: #d9d9d9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+  margin-left: 850px;
+`;
+
+export const Date = styled.div`
+  font-size: 10px;
+`;
+
+export const SumCharacter = styled.div`
+  font-size: 10px;
+  margin-top: 22px;
+  margin-left: 10px;
+`;
+
+export const LineImg = styled.img`
+  height: 25px;
+`;
+
+export const HeartWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  width: 126px;
+  height: 40px;
+  margin: 0px 50px 0px 75px;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  margin: 30px 0px 30px 0px;
+`;
+
+export const MiddelButtonWrapper = styled.div`
+  display: flex;
+  margin-left: 260px;
+`;
+
+export const MiddelButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  width: 99px;
+  height: 40px;
+  background-color: #2277d8;
+  color: white;
+  margin-right: 25px;
+`;
+
+export const TableLine = styled.div`
+  /* border-bottom: 1px solid #d9d9d9; */
+  border-bottom: 1px solid black;
+`;
+
+export const TableTop = styled.div`
+  display: flex;
+`;
+
+export const Borrow = styled.div`
+  width: 560px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Get = styled.div`
+  width: 560px;
+  height: 40px;
+  background-color: #ececec;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Square = styled.div`
+  width: 17px;
+  height: 17px;
+  background-color: #d9d9d9;
+  margin-top: 10px;
+`;
+
+export const SortButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 76px;
+  height: 30px;
+  border: 1px solid black;
+`;
+
+export const TableButton = styled.div`
+  width: 65px;
+  height: 30px;
+  background-color: #d9d9d9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+  margin-left: 10px;
+`;
+
+export const TableLineWrapper = styled.div`
+  display: flex;
+  padding: 10px;
+  margin-top: 10px;
+`;
+
+export const TableDottedLine = styled.div`
+  border-bottom: 1px dotted black;
+  padding: 10px;
+`;
+
+export const TableSonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+`;
+
+export const Margin = styled.div`
+  margin-bottom: 20px;
+`;
