0cdb8d5c8606a85a4c7c3ff5785bc80777d1ca3e
space086 <space_086@naver.com>
2022-07-06 18:11:57 +0900

    :등록:noticeListPage

---
+++ pages/noticeList/index.tsx
@@ -0,0 +1,5 @@
+import NoticeListContainer from "../../src/components/units/noticeList/NoticeList.containter";
+
+export default function NoticeListPage() {
+  return <NoticeListContainer />;
+}
---
+++ src/components/units/noticeList/NoticeList.styles.ts
@@ -0,0 +1,85 @@
+import styled from "@emotion/styled";
+
+export const Wrap = styled.div`
+  width: 1200px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+export const RowWrap = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+export const NoticeTitleWrap = styled.div``;
+export const Title = styled.label`
+  font-size: 30px;
+  font-weight: 700;
+  color: #484848;
+`;
+export const Subtitle = styled.p`
+  font-size: 18px;
+  color: #828282;
+  font-weight: 400;
+`;
+export const NoticeIcon = styled.img`
+  width: 188px;
+  height: 202px;
+`;
+
+export const NoticeListWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const Total = styled.span`
+  font-size: 15px;
+  color: #808080;
+`;
+export const NoticeWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  background-color: #fff;
+  border: 1px solid #bdbdbd;
+  box-shadow: 0px 4px 4px 0px #f2f2f2;
+  border-radius: 10px;
+  padding: 27px 43px;
+`;
+export const NoticeImg = styled.img`
+  width: 200px;
+  height: 120px;
+  background-color: #eee;
+`;
+export const TextWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-left: 35px;
+`;
+export const TextRowWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export const NoticeName = styled.label`
+  font-size: 24px;
+  font-weight: 700;
+  color: #484848;
+  margin-right: 40px;
+`;
+export const NoticeTag = styled.div`
+  background-color: #ddf0ff;
+  width: 50px;
+  height: 25px;
+  color: #2277d8;
+  text-align: center;
+`;
+export const CreatAt = styled.span`
+  font-size: 20px;
+  color: #808080;
+`;
+export const Ago = styled.span`
+  font-size: 20px;
+  color: #828282;
+`;
