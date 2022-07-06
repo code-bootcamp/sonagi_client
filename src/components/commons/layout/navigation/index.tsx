b05caaaaad0fbbf43596a22c4e1f81480b48c077
heejoo45890 <heejoo45890@gmail.com>
2022-07-06 11:20:30 +0900

    :수정:navigation

--- src/components/commons/layout/navigation/index.tsx
+++ src/components/commons/layout/navigation/index.tsx
@@ -3,7 +3,7 @@ export default function LayoutNavigation() {
   return (
     <>
       <S.Wrapper>
-        <S.ListTitle>최신작품</S.ListTitle>
+        <S.ListTitle>연재작품</S.ListTitle>
         <S.ListTitle>완결작품</S.ListTitle>
         <S.ListTitle>베스트셀러</S.ListTitle>
         <S.ListTitle>이벤트</S.ListTitle>
