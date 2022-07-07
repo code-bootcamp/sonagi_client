c1f03bbabc3cdb0c463c0cbdf857e183ea5c4604
heejoo45890 <heejoo45890@gmail.com>
2022-07-07 18:28:56 +0900

    :수정: signup 반응형

--- src/components/units/signup/signup.styles.ts
+++ src/components/units/signup/signup.styles.ts
@@ -4,6 +4,17 @@ export const Wrapper = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: row;
+
+  @media (min-width: 767px) and (max-width: 991px) {
+    min-width: 767px;
+    max-width: 991px;
+  }
+
+  @media (max-width: 767px) {
+    max-width: 767px;
+    width: 100vw;
+    justify-content: center;
+  }
 `;
 
 export const LoginWrapper = styled.div`
@@ -14,11 +25,31 @@ export const LoginWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+
+  @media (min-width: 767px) and (max-width: 991px) {
+    min-width: 767px;
+    max-width: 991px;
+    padding: 80px 30px 30px 30px;
+  }
+
+  @media (max-width: 767px) {
+    max-width: 767px;
+    width: 100vw;
+    padding: 50px 30px 30px 30px;
+  }
 `;
 
 export const PhoneImg = styled.img`
   width: 65vmax;
   height: 100vh;
+
+  @media (min-width: 767px) and (max-width: 991px) {
+    display: none;
+  }
+
+  @media (max-width: 767px) {
+    display: none;
+  }
 `;
 
 export const LogoImg = styled.img`
@@ -32,6 +63,10 @@ export const SubTitle = styled.div`
   text-align: center;
   letter-spacing: 0.232836px;
   color: #484848;
+
+  @media (max-width: 767px) {
+    display: none;
+  }
 `;
 
 export const InputWrapper = styled.div`
