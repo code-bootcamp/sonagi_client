a6352b12c185ad37c26f5b10952858ccd6369813
heejoo45890 <heejoo45890@gmail.com>
2022-07-08 20:16:18 +0900

    :수정: PointCharge 코드 리팩토링

--- src/components/units/myPage/pointCharge/PointCharge.container.tsx
+++ src/components/units/myPage/pointCharge/PointCharge.container.tsx
@@ -1,5 +1,89 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+import MyPageSideBarContainer from "../../../commons/myPageSideBar/MyPageSideBar.container";
 import PointChargePresenter from "./PointCharge.presenter";
 
+const Wrapper = styled.div`
+  display: flex;
+  width: 1200px;
+`;
+
+const Margin = styled.div`
+  margin: 30px 0px 0px 130px;
+`;
+
+declare const window: typeof globalThis & {
+  IMP: any;
+};
+
 export default function PointChargeContainer() {
-  return <PointChargePresenter />;
+  const Point = [2000, 5000, 10000, 20000, 50000, 100000, 200000];
+  const [value, setValue] = useState(0);
+
+  const onClickPoint = (el) => () => {
+    setValue(el);
+  };
+
+  const requestPay = () => {
+    console.log("dd");
+    // 3. 결제 준비하기 (init 안에는 가맹점 식별코드 넣어줌)
+    const IMP = window.IMP; // 생략 가능
+    // IMP.init("imp10159952"); // 예: imp00000000
+    IMP.init("imp63093514"); //
+
+    // 4. 결제 요청하기 (결제버튼 클릭 시 PG사의 결제 페이지가 열리게)
+    IMP.request_pay(
+      {
+        // param
+        pg: "html5_inicis",
+        pay_method: "card",
+        // merchant_uid: "ORD20180131-0000011",
+        // merchant_uid(상품 아이디) : 중복되면 안되기 때문에, 주석처리 해주면 자동으로 생성됨!
+        name: "포인트 충전",
+        amount: value,
+        buyer_email: "ee",
+        buyer_name: "name",
+        buyer_tel: "010-4242-4242",
+        buyer_addr: "서울특별시 구로구 구로동",
+        buyer_postcode: "01181",
+        // 근데 모바일에서 페이지에 들어간다면 결제 버튼 눌렀을 때 다른페이지로 이동해버림 그래서
+        // 6. 모바일 결제가 끝난 뒤 다시 돌아올 페이지 적어주기
+        m_redirect_url: "http://localhost:3000/mypage",
+      },
+      async (rsp: any) => {
+        // callback
+        if (rsp.success) {
+          // 결제 성공 시 로직,
+          try {
+            // const result = await createPointTransactionOfLoading({
+            //   variables: {
+            //     impUid: rsp.imp_uid,
+            //   },
+            // });
+            // console.log(result);
+            alert("충전완료");
+          } catch (error) {
+            console.log(error);
+          }
+        } else {
+          // 결제 실패 시 로직,
+          alert("결제 실패");
+          // ...
+        }
+      }
+    );
+  };
+  return (
+    <Wrapper>
+      <MyPageSideBarContainer />
+      <Margin>
+        <PointChargePresenter
+          Point={Point}
+          onClickPoint={onClickPoint}
+          value={value}
+          requestPay={requestPay}
+        />
+      </Margin>
+    </Wrapper>
+  );
 }
--- src/components/units/myPage/pointCharge/PointCharge.presenter.tsx
+++ src/components/units/myPage/pointCharge/PointCharge.presenter.tsx
@@ -1,7 +1,18 @@
+import Head from "next/head";
 import * as S from "./PointCharge.styles";
-export default function PointChargePresenter() {
+export default function PointChargePresenter(props) {
   return (
     <div>
+      <Head>
+        <script
+          type="text/javascript"
+          src="https://code.jquery.com/jquery-1.12.4.min.js"
+        ></script>
+        <script
+          type="text/javascript"
+          src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"
+        ></script>
+      </Head>
       <S.TopFlex>
         <S.MySonagiPoint>내 소나기 포인트</S.MySonagiPoint>
         <S.Flex>
@@ -11,71 +22,30 @@ export default function PointChargePresenter() {
       </S.TopFlex>
       <S.TableBorder>
         <S.TableTop>소나기 포인트 충전</S.TableTop>
-
-        <S.InnerTable>
-          <S.Input type="radio" name="cash" />
-          <S.InnerTableContents>소나기 포인트 2,000원</S.InnerTableContents>
-          <S.AmountFlex>
-            <S.HowMuchs>2,000</S.HowMuchs>
-            <div>원</div>
-          </S.AmountFlex>
-        </S.InnerTable>
-        <S.InnerTable>
-          <S.Input type="radio" name="cash" />
-          <S.InnerTableContents>소나기 포인트 5,000원</S.InnerTableContents>
-          <S.AmountFlex>
-            <S.HowMuchs>5,000</S.HowMuchs>
-            <div>원</div>
-          </S.AmountFlex>
-        </S.InnerTable>
-        <S.InnerTable>
-          <S.Input type="radio" name="cash" />
-          <S.InnerTableContents>소나기 포인트 10,000원</S.InnerTableContents>
-          <S.AmountFlex>
-            <S.HowMuch>10,000</S.HowMuch>
-            <div>원</div>
-          </S.AmountFlex>
-        </S.InnerTable>
-        <S.InnerTable>
-          <S.Input type="radio" name="cash" />
-          <S.InnerTableContents>소나기 포인트 20,000원</S.InnerTableContents>
-          <S.AmountFlex>
-            <S.HowMuch>20,000</S.HowMuch>
-            <div>원</div>
-          </S.AmountFlex>
-        </S.InnerTable>
-        <S.InnerTable>
-          <S.Input type="radio" name="cash" />
-          <S.InnerTableContents>소나기 포인트 50,000원</S.InnerTableContents>
-          <S.AmountFlex>
-            <S.HowMuch>50,000</S.HowMuch>
-            <div>원</div>
-          </S.AmountFlex>
-        </S.InnerTable>
-        <S.InnerTable>
-          <S.Input type="radio" name="cash" />
-          <S.InnerTableContents>소나기 포인트 100,000원</S.InnerTableContents>
-          <S.AmountFlexs>
-            <S.HowMuch>100,000</S.HowMuch>
-            <div>원</div>
-          </S.AmountFlexs>
-        </S.InnerTable>
-        <S.InnerTable>
-          <S.Input type="radio" name="cash" />
-          <S.InnerTableContents>소나기 포인트 200,000원</S.InnerTableContents>
-          <S.AmountFlexs>
-            <S.HowMuch>200,000</S.HowMuch>
-            <div>원</div>
-          </S.AmountFlexs>
-        </S.InnerTable>
+        {props.Point.map((el: any, index: any) => (
+          <S.InnerTable key={el.index} el={el} value={props.value}>
+            <S.Input
+              type="radio"
+              name="cash"
+              onClick={props.onClickPoint(el)}
+            />
+            <S.InnerTableContents>
+              소나기 포인트 {el.toLocaleString("ko-KR")}원
+            </S.InnerTableContents>
+            <S.AmountFlex>
+              <S.HowMuchs>{el.toLocaleString("ko-KR")}</S.HowMuchs>
+              <div>원</div>
+            </S.AmountFlex>
+          </S.InnerTable>
+        ))}
       </S.TableBorder>
       <S.FooterBar>
         <S.ChargeAmount>충전 금액</S.ChargeAmount>
         <S.FooterFlex>
-          <S.HowMuch>200,000</S.HowMuch>
+          <S.HowMuch>{props.value.toLocaleString("ko-KR")}</S.HowMuch>
           <S.FooterAmountWon>원</S.FooterAmountWon>
         </S.FooterFlex>
-        <S.ChargeButton>충전하기</S.ChargeButton>
+        <S.ChargeButton onClick={props.requestPay}>충전하기</S.ChargeButton>
       </S.FooterBar>
     </div>
   );
