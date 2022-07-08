f14ab3f1219400768fdf3b0a71e3ced31daa0763
heejoo45890 <heejoo45890@gmail.com>
2022-07-08 17:21:06 +0900

    :수정: freeboardList, freeboardWrite

--- src/components/units/freeBoard/freeBoardList/FreeBoardList.styles.ts
+++ src/components/units/freeBoard/freeBoardList/FreeBoardList.styles.ts
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   width: 100%;
+  height: 1700px;
   display: flex;
   flex-direction: column;
   padding-bottom: 50px;
--- src/components/units/freeBoard/freeBoardWrite/FreeBoardWrite.styles.tsx
+++ src/components/units/freeBoard/freeBoardWrite/FreeBoardWrite.styles.tsx
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   width: 100%;
+  height: 2000px;
   display: flex;
   flex-direction: column;
 `;
