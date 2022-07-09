85cff55c974bde0582e54eafcb3b5cdcfc361957
hskwon5170 <hskwon517@gmail.com>
2022-07-09 15:47:56 +0900

    :수정: ProfileCertify css수정(피그마 변경사항 추가, 배경이미지 추가), 반응형 디자인 적용

--- src/components/units/myPage/profileChange/ProfileCertify/ProfileCertify.presenter.tsx
+++ src/components/units/myPage/profileChange/ProfileCertify/ProfileCertify.presenter.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import * as S from "./ProfileCertify.styles";
 export default function ProfileCertifyPresenter() {
   return (
-    <S.Wrapper>
-      <S.MainTitle>회원정보 변경</S.MainTitle>
-      <S.WrapperPassword>
-        <S.WrapTitle>
-          <S.Label>보안을 위해 비밀번호를 한번 더 입력해 주세요</S.Label>
-          <S.WrapInput>
-            <S.LabelInput type="password" />
-            <S.Button>확인</S.Button>
-          </S.WrapInput>
-        </S.WrapTitle>
-      </S.WrapperPassword>
-    </S.Wrapper>
+    <S.Background>
+      <S.Wrapper>
+        <S.MainTitle>회원정보 변경</S.MainTitle>
+        <S.WrapperPassword>
+          <S.WrapTitle>
+            <S.Label>보안을 위해 비밀번호를 한번 더 입력해 주세요</S.Label>
+            <S.WrapInput>
+              <S.LabelInput type="password" />
+              <S.Button>확인</S.Button>
+            </S.WrapInput>
+          </S.WrapTitle>
+        </S.WrapperPassword>
+      </S.Wrapper>
+    </S.Background>
   );
 }
