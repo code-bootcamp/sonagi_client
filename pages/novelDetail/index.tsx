5a372059a15d976e0564a2c93460afc36cab0e12
heejoo45890 <heejoo45890@gmail.com>
2022-07-10 13:58:13 +0900

    :수정: novel 폴더구조, novelRead 화면, layout

--- pages/novelDetail/index.tsx
+++
@@ -1,5 +0,0 @@
-import NovelDetailPresenter from "../../src/components/units/novelDetailPage/NovelDetail.Presenter";
-
-export default function NovelDetailPage() {
-  return <NovelDetailPresenter />;
-}
