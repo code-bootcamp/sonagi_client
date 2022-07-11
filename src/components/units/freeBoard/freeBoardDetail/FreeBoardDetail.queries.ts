66474741554f5e9e4c764205d7b341168d7fb643
hskwon5170 <hskwon517@gmail.com>
2022-07-11 18:11:30 +0900

    :게시글 등록기능 구현, 특정 게시글 상세보기 페이지는 api 아직 구현 안되서 미완료상태

--- src/components/units/freeBoard/freeBoardDetail/FreeBoardDetail.queries.ts
+++ src/components/units/freeBoard/freeBoardDetail/FreeBoardDetail.queries.ts
@@ -3,13 +3,8 @@ import { gql } from "@apollo/client";
 export const FETCH_BOARDS = gql`
   query fetchBoards {
     fetchBoards {
-      id
       title
       contents
-      likeCount
-      viewCount
-      createAt
-      user
     }
   }
 `;
