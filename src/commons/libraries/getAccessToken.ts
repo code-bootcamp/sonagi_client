f4b1cf4b601e6554f3ffa728f6551bdfbcaa19ce
heejoo45890 <heejoo45890@gmail.com>
2022-07-11 11:42:49 +0900

    :수정: accesstoken

--- src/commons/libraries/getAccessToken.ts
+++ src/commons/libraries/getAccessToken.ts
@@ -4,10 +4,8 @@ import { GraphQLClient } from "graphql-request";
 
 // restoreAccessToken graphQl 발급받기
 const RESTORE_ACCESS_TOKEN = gql`
-  mutation restoreAccessToken {
-    restoreAccessToken {
-      accessToken
-    }
+  mutation {
+    restoreToken
   }
 `;
 
@@ -15,14 +13,13 @@ export async function getAccessToken() {
   try {
     // 1. GraphQlClient import
     const graphQlClient = new GraphQLClient(
-      "https://backend07.codebootcamp.co.kr/graphql",
+      "https://miny-shrimp.shop/graphql",
       { credentials: "include" }
     );
     // 2. RESTORE_ACCESS_TOKEN gql을 요청한 뒤 반환되는 결과값을 result에 담기
     const result = await graphQlClient.request(RESTORE_ACCESS_TOKEN);
     // 3. 재발급 받은 accessToken 을 newAccessToken에 저장하기
     const newAccessToken = result.restoreAccessToken.accessToken;
-
     return newAccessToken;
   } catch (error) {
     console.log(error.message);
