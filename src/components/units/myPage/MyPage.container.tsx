1a4b8b68dc40ef2212878ffd57d89515017011ba
heejoo45890 <heejoo45890@gmail.com>
2022-07-12 16:44:09 +0900

    :수정: Mypage

--- src/components/units/myPage/MyPage.container.tsx
+++ src/components/units/myPage/MyPage.container.tsx
@@ -1,57 +1,13 @@
-import { gql, useMutation, useQuery } from "@apollo/client";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { useRecoilState } from "recoil";
-import { accessTokenState } from "../../../commons/store";
+import { withAuth } from "../../../commons/hooks/withAuth";
 import MyPagePresenter from "./MyPage.presenter";
-import { FETCH_LOGIN_USER } from "./MyPage.queries";
-declare const window: typeof globalThis & {
-  IMP: any;
-};
 
-const LOGOUT_USER = gql`
-  mutation Logout {
-    Logout {
-      msg
-    }
-  }
-`;
-
-export default function MyPageContainer() {
+function MyPageContainer() {
   const router = useRouter();
-  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
-
   const [selected, setSelected] = useState(100);
   const [isSelect, setIsSelect] = useState(false);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const { data } = useQuery(FETCH_LOGIN_USER);
-  console.log("데이터", data);
-
-  const [Logout] = useMutation(LOGOUT_USER);
-  const onClickLogout = async () => {
-    const result = await Logout();
-    console.log(result);
-
-    localStorage.removeItem("refreshToken");
-    setAccessToken("");
-    window.location.replace("/");
-  };
-
-  const onClickMoveToLogin = () => {
-    console.log("로그인");
-    router.push("/login");
-  };
-
-  const onClickMoveToPaymentHistory = () => {
-    console.log("로그인");
-    router.push("/myPage/paymentHistory");
-  };
-
-  const onClickMoveToPaymentCharge = () => {
-    router.push("/myPage/paymentHistory");
-  };
-
   const onChangeAmount = (event: any) => {
     setSelected(event.target.value);
   };
@@ -63,54 +19,6 @@ export default function MyPageContainer() {
 
   const onClickMoveToPointCharge = () => {
     router.push("/myPage/pointCharge");
-  };
-
-  const SelectMoney = () => {
-    setIsSelect((prev) => !prev);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(true);
-  };
-
-  const requestpay = (data: any) => {
-    setIsModalVisible(false);
-
-    const IMP = window.IMP; // 생략 가능
-    IMP.init("imp49910675");
-
-    // 결제하기 화면
-    IMP.request_pay(
-      {
-        // param
-        pg: "html5_inicis",
-        pay_method: "card",
-        // merchant_uid: "ORD20180131-0000011", // 중복되면 안 됨
-        name: "포인트 충전",
-        amount: selected,
-        buyer_email: "gildong@gmail.com",
-        buyer_name: "홍길동",
-        buyer_tel: "010-4242-4242",
-        buyer_addr: "서울특별시 강남구 신사동",
-        buyer_postcode: "01181",
-        // 모바일 환경에서 이곳에 결제 후 이동할 페이지를 이곳에 넣어주기 (모바일에서 결제하면 다른페이지로 이동이 되어서 vs코드의 로직이 적용안됨)
-        // m_redirect_url: "http://localhost:3000/quiz/616quizPaySuccessPage",
-      },
-      (rsp: any) => {
-        // callback
-        // rsp:응답
-        if (rsp.success) {
-          console.log(rsp);
-          // 결제 성공 시 로직,
-          // ...
-        } else {
-          // 결제 실패 시 로직,
-          // ...
-          alert("결제에 실패했습니다. 다시 시도해주세요.");
-        }
-      }
-    );
-    console.log("충전하기 버튼을 누르셨습니다.");
   };
 
   return (
@@ -126,23 +34,15 @@ export default function MyPageContainer() {
         ></script>
       </Head>
       <MyPagePresenter
-        SelectMoney={SelectMoney}
         selected={selected}
         isSelect={isSelect}
         onClickMoney={onClickMoney}
-        requestpay={requestpay}
         onChangeAmount={onChangeAmount}
-        isModalVisible={isModalVisible}
-        handleOk={handleOk}
         onClickMoveToPointCharge={onClickMoveToPointCharge}
-        onClickMoveToPaymentCharge={onClickMoveToPaymentCharge}
-        data={data}
-        onClickLogout={onClickLogout}
-        accessToken={accessToken}
-        onClickMoveToLogin={onClickMoveToLogin}
-        onClickMoveToPaymentHistory={onClickMoveToPaymentHistory}
         // handleCancel={handleCancel}
       />
     </>
   );
 }
+
+export default withAuth(MyPageContainer);
--- src/components/units/myPage/pointCharge/PointCharge.container.tsx
+++ src/components/units/myPage/pointCharge/PointCharge.container.tsx
@@ -1,25 +1,32 @@
+import { useMutation, useQuery } from "@apollo/client";
 import { useState } from "react";
+import { FETCH_LOGIN_USER } from "../../../commons/layout/header";
 import PointChargePresenter from "./PointCharge.presenter";
+import { CREATE_PAYMENT, FETCH_PRODUCTS_ALL } from "./PointCharge.queries";
 
 declare const window: typeof globalThis & {
   IMP: any;
 };
 
 export default function PointChargeContainer() {
-  const Point = [2000, 5000, 10000, 20000, 50000, 100000, 200000];
+  const { data: loginData } = useQuery(FETCH_LOGIN_USER);
+  const { data: pointData } = useQuery(FETCH_PRODUCTS_ALL);
+  const [createPayment] = useMutation(CREATE_PAYMENT);
+
   const [value, setValue] = useState(0);
-  const onClickPoint = (el) => () => {
-    setValue(el);
+  const [amount, setAmount] = useState(0);
+  const [productID, setProductID] = useState("");
+
+  const onClickPoint = (el: any) => () => {
+    setValue(el.price);
+    setAmount(el.point);
+    setProductID(el.id);
   };
 
   const requestPay = () => {
-    console.log("dd");
-    // 3. 결제 준비하기 (init 안에는 가맹점 식별코드 넣어줌)
     const IMP = window.IMP; // 생략 가능
-    // IMP.init("imp10159952"); // 예: imp00000000
     IMP.init("imp63093514"); //
 
-    // 4. 결제 요청하기 (결제버튼 클릭 시 PG사의 결제 페이지가 열리게)
     IMP.request_pay(
       {
         // param
@@ -29,41 +36,40 @@ export default function PointChargeContainer() {
         // merchant_uid(상품 아이디) : 중복되면 안되기 때문에, 주석처리 해주면 자동으로 생성됨!
         name: "포인트 충전",
         amount: value,
-        buyer_email: "ee",
-        buyer_name: "name",
-        buyer_tel: "010-4242-4242",
+        buyer_email: loginData?.fetchLoginUser.email,
+        buyer_name: loginData?.fetchLoginUser.name,
+        buyer_tel: loginData?.fetchLoginUser.phone,
         buyer_addr: "서울특별시 구로구 구로동",
         buyer_postcode: "01181",
-        // 근데 모바일에서 페이지에 들어간다면 결제 버튼 눌렀을 때 다른페이지로 이동해버림 그래서
-        // 6. 모바일 결제가 끝난 뒤 다시 돌아올 페이지 적어주기
         m_redirect_url: "http://localhost:3000/mypage",
       },
       async (rsp: any) => {
-        // callback
         if (rsp.success) {
-          // 결제 성공 시 로직,
           try {
-            // const result = await createPointTransactionOfLoading({
-            //   variables: {
-            //     impUid: rsp.imp_uid,
-            //   },
-            // });
-            // console.log(result);
+            const result = await createPayment({
+              variables: {
+                createPaymentInput: {
+                  impUid: rsp.imp_uid,
+                  merchantUid: rsp.merchant_uid,
+                  amount,
+                  productID,
+                },
+              },
+            });
+            console.log(result);
             alert("충전완료");
           } catch (error) {
             console.log(error);
           }
         } else {
-          // 결제 실패 시 로직,
           alert("결제 실패");
-          // ...
         }
       }
     );
   };
   return (
     <PointChargePresenter
-      Point={Point}
+      pointData={pointData}
       onClickPoint={onClickPoint}
       value={value}
       requestPay={requestPay}
