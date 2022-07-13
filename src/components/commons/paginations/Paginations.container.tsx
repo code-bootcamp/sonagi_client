002c9ba83bf1c3b3df37a7f829e75d07fbe3ad4f
hskwon5170 <hskwon517@gmail.com>
2022-07-13 16:16:45 +0900

    :등록: 페이지네이션 수정중

--- src/components/commons/paginations/Paginations.container.tsx
+++ src/components/commons/paginations/Paginations.container.tsx
@@ -1,40 +1,23 @@
-import { MouseEvent, useState } from "react";
-import PaginationsUI from "./Paginations.presenter";
+import React, { useState } from "react";
+import Pagination from "react-js-pagination";
 
-export default function Paginations(props: any) {
-  const [startPage, setStartPage] = useState(1);
-  const [activedPage, setActivedPage] = useState(1);
-  const lastPage = props.count ? Math.ceil(props.count / 10) : 0;
+export const Paging = () => {
+  const [page, setPage] = useState(1);
 
-  const onClickPage = (event: MouseEvent<HTMLSpanElement>) => {
-    if (!(event.target instanceof Element)) return;
-    const activedPage = Number(event.target.id);
-    setActivedPage(activedPage);
-    props.refetch({ page: activedPage });
-  };
-
-  const onClickPrevPage = () => {
-    if (startPage <= 1) return;
-    setStartPage((prev) => prev - 10);
-    setActivedPage(startPage - 10);
-    props.refetch({ page: startPage - 10 });
-  };
-
-  const onClickNextPage = () => {
-    if (startPage + 10 > lastPage) return;
-    setStartPage((prev) => prev + 10);
-    setActivedPage(startPage + 10);
-    props.refetch({ page: startPage + 10 });
+  const handlePageChange = (page) => {
+    setPage(page);
+    console.log(page);
   };
 
   return (
-    <PaginationsUI
-      startPage={startPage}
-      lastPage={lastPage}
-      activedPage={activedPage}
-      onClickPage={onClickPage}
-      onClickPrevPage={onClickPrevPage}
-      onClickNextPage={onClickNextPage}
+    <Pagination
+      activePage={page}
+      itemsCountPerPage={10}
+      totalItemsCount={450}
+      pageRangeDisplayed={5}
+      prevPageText={"‹"}
+      nextPageText={"›"}
+      onChange={handlePageChange}
     />
   );
-}
+};
