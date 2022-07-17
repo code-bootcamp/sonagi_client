ae2226e49e66a67cfd88b079d2ec5aea3103fdd9
space086 <space_086@naver.com>
2022-07-17 20:55:30 +0900

    :수정: novelList query 수정

--- src/components/units/novelList/publish/NovelList.presenter.tsx
+++ src/components/units/novelList/publish/NovelList.presenter.tsx
@@ -12,30 +12,36 @@ export default function NovelPbListPresenter(props: any) {
         <S.Arrow src="/novelList/arrow.png" />
       </S.RowWrap>
       <S.GridWrap>
-        {props.data?.fetchNovelsPage?.novels.slice(0, 5).map((el: any) => (
-          <S.ItemWrap
-            onClick={props.onClickMoveToDetail}
-            id={el.id}
-            key={el.title}
-          >
-            <S.ItemPic
-              src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
-            />
-            <S.ItemInfo>
-              <S.ItemName>{el.title}</S.ItemName>
-              <S.ItemNum>10화</S.ItemNum>
-            </S.ItemInfo>
-            <S.ItemWriterWrap>
-              <S.WriterIcon src="/novelList/writer-icon.png" />
-              <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
-            </S.ItemWriterWrap>
-            <S.Contents
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(el.description),
-              }}
-            ></S.Contents>
-          </S.ItemWrap>
-        ))}
+        {props.data?.fetchNovelCyclesPageLastOrder?.novels
+          .slice(0, 5)
+          .map((el: any) => (
+            <S.ItemWrap
+              onClick={props.onClickMoveToDetail}
+              id={el.id}
+              key={el.title}
+            >
+              <S.ItemPic
+                src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
+              />
+              <S.ItemInfo>
+                <S.ItemName>{el.title}</S.ItemName>
+                {el.cycle ? (
+                  <S.Cycle>자유연재</S.Cycle>
+                ) : (
+                  <S.Cycle>요일연재</S.Cycle>
+                )}
+              </S.ItemInfo>
+              <S.ItemWriterWrap>
+                <S.WriterIcon src="/novelList/writer-icon.png" />
+                <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
+              </S.ItemWriterWrap>
+              <S.Contents
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(el.description),
+                }}
+              ></S.Contents>
+            </S.ItemWrap>
+          ))}
       </S.GridWrap>
       <S.Line></S.Line>
       {/* 로맨스판타지 */}
@@ -44,26 +50,28 @@ export default function NovelPbListPresenter(props: any) {
         <S.Arrow src="/novelList/arrow.png" />
       </S.RowWrap>
       <S.GridWrap>
-        {props.data?.fetchNovelsPage.novels.slice(0, 5).map((el: any) => (
-          <S.ItemWrap key={el.id}>
-            <S.ItemPic
-              src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
-            />
-            <S.ItemInfo>
-              <S.ItemName>{el.title}</S.ItemName>
-              <S.ItemNum>10화</S.ItemNum>
-            </S.ItemInfo>
-            <S.ItemWriterWrap>
-              <S.WriterIcon src="/novelList/writer-icon.png" />
-              <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
-            </S.ItemWriterWrap>
-            <S.Contents
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(el.description),
-              }}
-            ></S.Contents>
-          </S.ItemWrap>
-        ))}
+        {props.data?.fetchNovelCyclesPageLastOrder.novels
+          .slice(0, 5)
+          .map((el: any) => (
+            <S.ItemWrap key={el.id}>
+              <S.ItemPic
+                src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
+              />
+              <S.ItemInfo>
+                <S.ItemName>{el.title}</S.ItemName>
+                <S.ItemNum>10화</S.ItemNum>
+              </S.ItemInfo>
+              <S.ItemWriterWrap>
+                <S.WriterIcon src="/novelList/writer-icon.png" />
+                <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
+              </S.ItemWriterWrap>
+              <S.Contents
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(el.description),
+                }}
+              ></S.Contents>
+            </S.ItemWrap>
+          ))}
       </S.GridWrap>
       <S.Line></S.Line>
       {/* 판타지 */}
@@ -72,27 +80,29 @@ export default function NovelPbListPresenter(props: any) {
         <S.Arrow src="/novelList/arrow.png" />
       </S.RowWrap>
       <S.GridWrap>
-        {props.data?.fetchNovelsPage.novels.slice(0, 5).map((el: any) => (
-          <S.ItemWrap key={el.id}>
-            <S.ItemPic
-              src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
-            />
-            <S.ItemInfo>
-              <S.ItemName>{el.title}</S.ItemName>
-              <S.ItemNum>10화</S.ItemNum>
-            </S.ItemInfo>
+        {props.data?.fetchNovelCyclesPageLastOrder.novels
+          .slice(0, 5)
+          .map((el: any) => (
+            <S.ItemWrap key={el.id}>
+              <S.ItemPic
+                src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
+              />
+              <S.ItemInfo>
+                <S.ItemName>{el.title}</S.ItemName>
+                <S.ItemNum>10화</S.ItemNum>
+              </S.ItemInfo>
 
-            <S.ItemWriterWrap>
-              <S.WriterIcon src="/novelList/writer-icon.png" />
-              <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
-            </S.ItemWriterWrap>
-            <S.Contents
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(el.description),
-              }}
-            ></S.Contents>
-          </S.ItemWrap>
-        ))}
+              <S.ItemWriterWrap>
+                <S.WriterIcon src="/novelList/writer-icon.png" />
+                <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
+              </S.ItemWriterWrap>
+              <S.Contents
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(el.description),
+                }}
+              ></S.Contents>
+            </S.ItemWrap>
+          ))}
       </S.GridWrap>
       <S.Line></S.Line>
       {/* 무협 */}
@@ -101,26 +111,28 @@ export default function NovelPbListPresenter(props: any) {
         <S.Arrow src="/novelList/arrow.png" />
       </S.RowWrap>
       <S.GridWrap>
-        {props.data?.fetchNovelsPage.novels.slice(0, 5).map((el: any) => (
-          <S.ItemWrap key={el.id}>
-            <S.ItemPic
-              src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
-            />
-            <S.ItemInfo>
-              <S.ItemName>{el.title}</S.ItemName>
-              <S.ItemNum>10화</S.ItemNum>
-            </S.ItemInfo>
-            <S.ItemWriterWrap>
-              <S.WriterIcon src="/novelList/writer-icon.png" />
-              <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
-            </S.ItemWriterWrap>
-            <S.Contents
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(el.description),
-              }}
-            ></S.Contents>
-          </S.ItemWrap>
-        ))}
+        {props.data?.fetchNovelCyclesPageLastOrder.novels
+          .slice(0, 5)
+          .map((el: any) => (
+            <S.ItemWrap key={el.id}>
+              <S.ItemPic
+                src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
+              />
+              <S.ItemInfo>
+                <S.ItemName>{el.title}</S.ItemName>
+                <S.ItemNum>10화</S.ItemNum>
+              </S.ItemInfo>
+              <S.ItemWriterWrap>
+                <S.WriterIcon src="/novelList/writer-icon.png" />
+                <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
+              </S.ItemWriterWrap>
+              <S.Contents
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(el.description),
+                }}
+              ></S.Contents>
+            </S.ItemWrap>
+          ))}
       </S.GridWrap>
       <S.Line></S.Line>
       {/* 학원 */}
@@ -129,26 +141,28 @@ export default function NovelPbListPresenter(props: any) {
         <S.Arrow src="/novelList/arrow.png" />
       </S.RowWrap>
       <S.GridWrap>
-        {props.data?.fetchNovelsPage.novels.slice(0, 5).map((el: any) => (
-          <S.ItemWrap key={el.id}>
-            <S.ItemPic
-              src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
-            />
-            <S.ItemInfo>
-              <S.ItemName>{el.title}</S.ItemName>
-              <S.ItemNum>10화</S.ItemNum>
-            </S.ItemInfo>
-            <S.ItemWriterWrap>
-              <S.WriterIcon src="/novelList/writer-icon.png" />
-              <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
-            </S.ItemWriterWrap>
-            <S.Contents
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(el.description),
-              }}
-            ></S.Contents>
-          </S.ItemWrap>
-        ))}
+        {props.data?.fetchNovelCyclesPageLastOrder.novels
+          .slice(0, 5)
+          .map((el: any) => (
+            <S.ItemWrap key={el.id}>
+              <S.ItemPic
+                src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
+              />
+              <S.ItemInfo>
+                <S.ItemName>{el.title}</S.ItemName>
+                <S.ItemNum>10화</S.ItemNum>
+              </S.ItemInfo>
+              <S.ItemWriterWrap>
+                <S.WriterIcon src="/novelList/writer-icon.png" />
+                <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
+              </S.ItemWriterWrap>
+              <S.Contents
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(el.description),
+                }}
+              ></S.Contents>
+            </S.ItemWrap>
+          ))}
       </S.GridWrap>
       <S.Line></S.Line>
 
@@ -158,26 +172,28 @@ export default function NovelPbListPresenter(props: any) {
         <S.Arrow src="/novelList/arrow.png" />
       </S.RowWrap>
       <S.GridWrap>
-        {props.data?.fetchNovelsPage.novels.slice(0, 5).map((el: any) => (
-          <S.ItemWrap key={el.id}>
-            <S.ItemPic
-              src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
-            />
-            <S.ItemInfo>
-              <S.ItemName>{el.title}</S.ItemName>
-              <S.ItemNum>10화</S.ItemNum>
-            </S.ItemInfo>
-            <S.ItemWriterWrap>
-              <S.WriterIcon src="/novelList/writer-icon.png" />
-              <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
-            </S.ItemWriterWrap>
-            <S.Contents
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(el.description),
-              }}
-            ></S.Contents>
-          </S.ItemWrap>
-        ))}
+        {props.data?.fetchNovelCyclesPageLastOrder.novels
+          .slice(0, 5)
+          .map((el: any) => (
+            <S.ItemWrap key={el.id}>
+              <S.ItemPic
+                src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
+              />
+              <S.ItemInfo>
+                <S.ItemName>{el.title}</S.ItemName>
+                <S.ItemNum>{el.cycle}</S.ItemNum>
+              </S.ItemInfo>
+              <S.ItemWriterWrap>
+                <S.WriterIcon src="/novelList/writer-icon.png" />
+                <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
+              </S.ItemWriterWrap>
+              <S.Contents
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(el.description),
+                }}
+              ></S.Contents>
+            </S.ItemWrap>
+          ))}
       </S.GridWrap>
     </S.Wrap>
   );
--- src/components/units/novelList/publish/NovelList.queries.ts
+++ src/components/units/novelList/publish/NovelList.queries.ts
@@ -1,14 +1,18 @@
 import { gql } from "@apollo/client";
 
-export const FETCH_NOVELS_PAGE = gql`
-  query fetchNovelsPage($page: Int!) {
-    fetchNovelsPage(page: $page) {
+export const FETCH_PB_NOVELS_PAGE = gql`
+  query fetchNovelCyclesPageLastOrder($page: Int!) {
+    fetchNovelCyclesPageLastOrder(page: $page) {
       novels {
         id
         title
         description
+        cycle
         user {
           nickName
+        }
+        novelCategory {
+          name
         }
         files {
           url
