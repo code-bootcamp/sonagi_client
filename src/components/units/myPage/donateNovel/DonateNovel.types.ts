f8b8ecb9c5884e1b65cf188cb996e3d9c76269fc
heejoo45890 <heejoo45890@gmail.com>
2022-07-20 15:02:10 +0900

    :수정: myPage donatePayments

--- src/components/units/myPage/donateNovel/DonateNovel.types.ts
+++ src/components/units/myPage/donateNovel/DonateNovel.types.ts
@@ -3,10 +3,14 @@ import { Key } from "react";
 export interface IDonateNovelContainerProps {}
 
 export interface IDonateNovelPresenterProps {
+  setIsDonate: any;
+  isDonate: any;
+  Donate: boolean;
   point: any;
   novel: any;
   id: Key | null | undefined;
   data: any;
   el?: any;
   onClickMoveToPage: any;
+  onClickDonate: any;
 }
