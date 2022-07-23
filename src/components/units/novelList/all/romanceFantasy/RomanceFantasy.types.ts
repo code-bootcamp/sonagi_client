ccf256910bb9a09ce82ad84397438cca7e626e3b
heejoo45890 <heejoo45890@gmail.com>
2022-07-23 15:17:25 +0900

    :수정: error

--- src/components/units/novelList/all/romanceFantasy/RomanceFantasy.types.ts
+++ src/components/units/novelList/all/romanceFantasy/RomanceFantasy.types.ts
@@ -1,1 +1,30 @@
-export interface IRomanceFantasyPresenterProps {}
+import { MouseEvent } from "react";
+interface IFiles {
+  url: string;
+}
+export interface Iel {
+  cycle?: number;
+  description?: string;
+  files?: Array<IFiles>;
+  id?: string;
+  isFinish?: boolean;
+  likeCount?: number;
+  novelCategory?: {
+    name: string;
+  };
+  title?: string;
+  user?: {
+    id: string;
+    name: string;
+    nickName: string;
+  };
+  viewCount?: number;
+  __typename?: string;
+}
+
+export interface IRomanceFantasyPresenterProps {
+  PbRomanceFantasy: any;
+  FinRomanceFantasy: any;
+  data?: any;
+  onClickMoveToDetail: (el: Iel) => (event: MouseEvent<HTMLDivElement>) => void;
+}
