d4699807c500fcc67bf30481020a59d67533b87d
space086 <space_086@naver.com>
2022-07-30 14:35:22 +0900

    :수정: 토큰수정

--- src/commons/libraries/getAccessToken.ts
+++ src/commons/libraries/getAccessToken.ts
@@ -11,7 +11,7 @@ const RESTORE_ACCESS_TOKEN = gql`
 export async function getAccessToken() {
   try {
     const graphQlClient = new GraphQLClient(
-      "https://miny-shrimp.shop/graphqll",
+      "https://miny-shrimp.shop/graphql",
       { credentials: "include" }
     );
     const result = await graphQlClient.request(RESTORE_ACCESS_TOKEN);
