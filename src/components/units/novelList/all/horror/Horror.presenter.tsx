f483d9755ec0fb6e483f90723882df436b680619
space086 <space_086@naver.com>
2022-08-01 14:15:56 +0900

    :수정: 장르 오류 수정

--- src/components/units/novelList/all/horror/Horror.presenter.tsx
+++ src/components/units/novelList/all/horror/Horror.presenter.tsx
@@ -5,37 +5,39 @@ import { IHorrorPresenterProps } from "./Horror.types";
 export default function HorrorPresenter(props: IHorrorPresenterProps) {
   return (
     <S.Wrap>
-      <S.Title>공포/미스터리</S.Title>
+      <S.Title>공포/미스터리test</S.Title>
       {/* 연재중 */}
       <S.RowWrap>
         <S.Category>연재작품</S.Category>
         <S.Arrow src="/novelList/arrow.png" />
       </S.RowWrap>
       <S.GridWrap>
-        {props.PbHorror?.fetchNovelsPage?.novels.map((el: any) => (
-          <S.ItemWrap
-            onClick={props.onClickMoveToDetail}
-            id={el.id}
-            key={el.title}
-          >
-            <S.ItemPic
-              src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
-            />
-            <S.ItemInfo>
-              <S.ItemName>{el.title}</S.ItemName>
-              <S.ItemNum>10화</S.ItemNum>
-            </S.ItemInfo>
-            <S.ItemWriterWrap>
-              <S.WriterIcon src="/novelList/writer-icon.png" />
-              <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
-            </S.ItemWriterWrap>
-            <S.Contents
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(el.description),
-              }}
-            ></S.Contents>
-          </S.ItemWrap>
-        ))}
+        {props.PbHorror?.fetchNovelsPage?.novels
+          .filter((v: any) => v)
+          .map((el: any) => (
+            <S.ItemWrap
+              onClick={props.onClickMoveToDetail(el)}
+              id={el.id}
+              key={el.title}
+            >
+              <S.ItemPic
+                src={`https://storage.googleapis.com/code-camp-main-project/${el.files[0]?.url}`}
+              />
+              <S.ItemInfo>
+                <S.ItemName>{el.title}</S.ItemName>
+                <S.ItemNum>10화</S.ItemNum>
+              </S.ItemInfo>
+              <S.ItemWriterWrap>
+                <S.WriterIcon src="/novelList/writer-icon.png" />
+                <S.WrtiterName>{el.user?.nickName}</S.WrtiterName>
+              </S.ItemWriterWrap>
+              <S.Contents
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(el.description),
+                }}
+              ></S.Contents>
+            </S.ItemWrap>
+          ))}
       </S.GridWrap>
       <S.Line></S.Line>
       {/* 완결작 */}
